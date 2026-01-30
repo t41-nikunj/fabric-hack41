3fe49534195594c8277c81e5a87f07d0fd9f4580
sujalthink41 <sujal.kumar@think41.com>
2026-01-30 14:02:18 +0530

    skills: qa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <h1 className='text-3xl text-center text-red-400'>Hack41</h1>
+      <h1 className='text-5xl text-center text-red-400'>Hack41</h1>
     </>
   )
 }
